f3003a452d0b8426e5bd57603b5e6b4a8faa3ed5
Keanelek Enns <keanelekenns@gmail.com>
2022-02-23 17:15:27 -0800

    reduce unnecessary code

--- classification.py
+++ classification.py
@@ -3,15 +3,18 @@ import argparse
 import utils
 from sklearn.svm import LinearSVC
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import StratifiedKFold
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
+
+A_LABEL = "A"
+B_LABEL = "B"
 
 def evaluate_classifier(confusion_matrix):
     """
     Inputs:
         confusion_matrix - returned by confusion_matrix in sklearn
-        The labels are as follows: Class A = 0, Class B = 1.
     Returns:
         Accuracy, Precision, Recall, F1 - Classifier metrics as defined by 
         https://towardsdatascience.com/classification-performance-metrics-69c69ab03f17
@@ -28,63 +31,28 @@ def evaluate_classifier(confusion_matrix):
 
     return accuracy, precision, recall, f1
 
-def train_cs_p1_classifier(cs_a_b, cs_b_a, train, labels):
+def cs_p1_graphs_to_points(graphs, cs_a_b, cs_b_a):
     """
-    Fit a classifier with the train data using contrast subgraphs
+    Uses the CS-P1 method for creating data points from brain graphs
     Inputs:
-        cs_a_b - A 1D numpy array representing the contrast subgraph generated from
-        finding a dense subgraph in the difference graph summary_A - summary_B.
-        cs_b_a - Similar to cs_a_b, only the difference graph used to generate it was
-        summary_B - summary_A.
-        train - A 3D numpy array representing a group of brain graphs in classes A and B.
-        labels - A 1D numpy array representing class labels for each graph in train.
-    Returns:
-        A trained Linear SVC Classifier from sklearn
-    """
-    points = np.array(list(map(lambda graph: cs_p1_graph_to_point(graph, cs_a_b, cs_b_a), train)))
-    lsvc = LinearSVC()
-    lsvc.fit(points, labels)
-    return lsvc
-
-def cs_p1(cs_a_b, cs_b_a, test, classifier):
-    """
-    Uses the CS-P1 method for classifying a test set of brain graphs
-    Inputs:
-        cs_a_b - A 1D numpy array representing the contrast subgraph generated from
-        finding a dense subgraph in the difference graph summary_A - summary_B.
-        cs_b_a - Similar to cs_a_b, only the difference graph used to generate it was
-        summary_B - summary_A.
-        test - A 3D numpy array representing a group of brain graphs in classes A and B.
-        classifier - A trained linear SVC classifier from sklearn
-    Returns:
-        predictions - A 1D numpy array holding prediction values for each graph in test.
-        The prediction values are as follows: Class A = 0, Class B = 1.
-    """
-    points = np.array(list(map(lambda graph: cs_p1_graph_to_point(graph, cs_a_b, cs_b_a), test)))
-    return classifier.predict(points)
-
-def cs_p1_graph_to_point(graph, cs_a_b, cs_b_a):
-    """
-    Uses the CS-P1 method for creating a data point from a brain graph
-    Inputs:
-        graph - A 2D numpy array representing a brain graph adjacency matrix.
+        graphs - A 3D numpy array representing brain graphs.
         cs_a_b - A 1D numpy array representing the contrast subgraph generated from
         finding a dense subgraph in the difference graph summary_A - summary_B.
         cs_b_a - Similar to cs_a_b, only the difference graph used to generate it was
         summary_B - summary_A.
     Returns:
-        point - A 1D numpy array of length 2 representing a coordinate according to the CS-P1
-        formulation.
+        points - A 2D numpy array representing graph coordinates according to the CS-P1 formulation.
     """
-    return np.array([
-        utils.contrast_subgraph_overlap(graph, cs_a_b),
-        utils.contrast_subgraph_overlap(graph, cs_b_a)])
+    return np.array(list(map(lambda graph:
+                                np.array([utils.contrast_subgraph_overlap(graph, cs_a_b),
+                                          utils.contrast_subgraph_overlap(graph, cs_b_a)]),
+                             graphs)))
 
-def cs_p2_graph_to_point(graph, contrast_subgraph, summary_A, summary_B):
+def cs_p2_graphs_to_points(graphs, contrast_subgraph, summary_A, summary_B):
     """
-    Uses the CS-P2 method for creating a data point from a brain graph
+    Uses the CS-P2 method for creating data points from brain graphs
     Inputs:
-        graph - A 2D numpy array representing a brain graph adjacency matrix.
+        graphs - A 3D numpy array representing brain graphs.
         contrast_subgraph - A 1D numpy array representing the contrast subgraph generated from
         finding a dense subgraph in the absolute difference graph abs(summary_A - summary_B).
         summary_A - A 2D numpy array with the shape of a brain graph, where each entry is the
@@ -92,16 +60,15 @@ def cs_p2_graph_to_point(graph, contrast_subgraph, summary_A, summary_B):
         summary_B - A 2D numpy array with the shape of a brain graph, where each entry is the
         percentage of graphs in class B that contain that given edge.
     Returns:
-        point - A 1D numpy array of length 2 representing a coordinate according to the CS-P2
-        formulation.
+        points - A 2D numpy array representing graph coordinates according to the CS-P2 formulation.
     """
-    induced_subgraph = utils.induce_subgraph(graph, contrast_subgraph)
-    return np.array([
-        utils.l1_norm(induced_subgraph,
-                      utils.induce_subgraph(summary_B, contrast_subgraph)),
-        utils.l1_norm(induced_subgraph,
-                      utils.induce_subgraph(summary_A, contrast_subgraph))
-    ])
+    return np.array(list(map(lambda graph:
+                                np.array([
+                                    utils.l1_norm(utils.induce_subgraph(graph, contrast_subgraph),
+                                                utils.induce_subgraph(summary_B, contrast_subgraph)),
+                                    utils.l1_norm(utils.induce_subgraph(graph, contrast_subgraph),
+                                                utils.induce_subgraph(summary_A, contrast_subgraph))]),
+                             graphs)))
 
 def plot_cs_p1(cs_a_b, cs_b_a, graphs_A, graphs_B):
 
@@ -135,13 +102,12 @@ def get_AB_labels(graphs_A, graphs_B):
         graphs_B - A 3D numpy array representing a group of brain graphs in class B.
     Returns:
         graphs - A 3D numpy array representing a group of brain graphs in classes A and B.
-        labels - A 1D numpy array holding class labels for each graph in train.
-        The label values are as follows: Class A = 0, Class B = 1.
+        labels - A list holding class labels for each graph in train.
     """
-    labels_A = np.zeros(len(graphs_A))
-    labels_B = np.ones(len(graphs_B))
+    labels_A = [A_LABEL]*len(graphs_A)
+    labels_B = [B_LABEL]*len(graphs_B)
     graphs = np.concatenate((graphs_A, graphs_B))
-    labels = np.concatenate((labels_A, labels_B))
+    labels = labels_A + labels_B
     return graphs, labels
 
 def main():
@@ -167,6 +133,8 @@ def main():
 
     train_graphs, train_labels = get_AB_labels(train_A, train_B)
     test_graphs, test_labels = get_AB_labels(test_A, test_B)
+    
+    classifier = LinearSVC()
 
     # Get the difference network between the edge weights in group A and B
     if args.p == 1:
@@ -179,10 +147,10 @@ def main():
         cs_a_b = np.array([4, 6, 8, 9, 13, 15, 92, 88, 60])
         # Children ASD-TD alpha = 0.8
         cs_b_a = np.array([36, 37, 71, 41, 74, 76, 77, 79, 81, 55, 38, 95])
-    
-        classifier = train_cs_p1_classifier(cs_a_b, cs_b_a, train_graphs, train_labels)
-        test_pred = cs_p1(cs_a_b, cs_b_a, test_graphs, classifier)
-        print(classification_report(test_labels, test_pred, target_names=["A", "B"]))
+
+        classifier.fit(cs_p1_graphs_to_points(train_graphs, cs_a_b, cs_b_a), train_labels)
+        test_pred = classifier.predict(cs_p1_graphs_to_points(test_graphs, cs_a_b, cs_b_a))
+        print(classification_report(test_labels, test_pred))
         print(confusion_matrix(test_labels, test_pred))
         print(evaluate_classifier(confusion_matrix(test_labels, test_pred)))
     else:
@@ -190,6 +158,14 @@ def main():
         # Call function(s) for generating contrast subgraph
 
         # TEMPORARILY HARDCODED
+        cs = np.array([4, 6, 8, 9, 13, 15, 92, 88, 60])
+
+        classifier.fit(cs_p2_graphs_to_points(train_graphs, cs, summary_A, summary_B), train_labels)
+        test_pred = classifier.predict(cs_p2_graphs_to_points(test_graphs, cs, summary_A, summary_B))
+        print(classification_report(test_labels, test_pred))
+        print(confusion_matrix(test_labels, test_pred))
+        print(evaluate_classifier(confusion_matrix(test_labels, test_pred)))
+
         
 
 
