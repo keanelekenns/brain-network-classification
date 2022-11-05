5ba722a786bf00dcf33720395700d181f0e64683
Keanelek Enns <>
2022-11-05 00:10:56 -0700

    change child criteria to match lanciano paper

--- scripts/calculate_unique_subjects.py
+++ scripts/calculate_unique_subjects.py
@@ -4,10 +4,24 @@ import os
 def write_unique_subject_filepaths(src_dir, output_filename):
 
     obj = {}
+    asd_count = 0
+    td_count = 0
 
     for dirpath, _, filenames in os.walk(src_dir):
+        if "/asd" not in dirpath and "/td" not in dirpath:
+            continue
+        
+        count = 0
         for filename in filenames:
+            if filename in obj:
+                count -= 1 # don't count repeats
             obj[filename] = dirpath
+            count += 1
+        
+        if "/asd" in dirpath:
+            asd_count += count
+        elif "/td" in dirpath:
+            td_count += count
 
     filepaths = []
     for filename, dirpath in obj.items():
@@ -17,7 +31,7 @@ def write_unique_subject_filepaths(src_dir, output_filename):
     with open(output_filename, "w") as fp:
         fp.write(json.dumps(filepaths))
 
-    print(f"found {len(filepaths)} unique subjects in {src_dir}")
+    print(f"found {len(filepaths)} (ASD = {asd_count}, TD = {td_count}) unique subjects in {src_dir}")
 
 
 
