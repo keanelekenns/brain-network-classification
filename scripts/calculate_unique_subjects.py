138e2dc8944363f6eff953d9a2b93aad78563601
Keanelek Enns <keanelekenns@gmail.com>
2022-12-10 00:59:54 -0800

    update pipelines and unique script

--- scripts/calculate_unique_subjects.py
+++ scripts/calculate_unique_subjects.py
@@ -3,46 +3,55 @@ import os
 
 def write_unique_subject_filepaths(src_dir, output_filename):
 
-    obj = {}
+    asd_files = {}
     asd_count = 0
+    td_files = {}
     td_count = 0
 
     for dirpath, _, filenames in os.walk(src_dir):
-        if "/asd" not in dirpath and "/td" not in dirpath:
-            continue
-        
-        count = 0
-        for filename in filenames:
-            if filename in obj:
-                count -= 1 # don't count repeats
-            obj[filename] = dirpath
-            count += 1
-        
+
         if "/asd" in dirpath:
-            asd_count += count
+            for filename in filenames:
+                if filename in asd_files:
+                    asd_count -= 1 # don't count repeats
+                asd_files[filename] = dirpath
+                asd_count += 1
         elif "/td" in dirpath:
-            td_count += count
+            for filename in filenames:
+                if filename in td_files:
+                    td_count -= 1 # don't count repeats
+                td_files[filename] = dirpath
+                td_count += 1
+        
 
-    filepaths = []
-    for filename, dirpath in obj.items():
-        filepaths.append(dirpath + "/" + filename)
+    asd_filepaths = []
+    for filename, dirpath in asd_files.items():
+        asd_filepaths.append(dirpath + "/" + filename)
 
+    td_filepaths = []
+    for filename, dirpath in td_files.items():
+        td_filepaths.append(dirpath + "/" + filename)
+
+    filepaths = {
+        "ASD" : asd_filepaths,
+        "TD" : td_filepaths
+    }
 
     with open(output_filename, "w") as fp:
         fp.write(json.dumps(filepaths))
 
-    print(f"found {len(filepaths)} (ASD = {asd_count}, TD = {td_count}) unique subjects in {src_dir}")
+    print(f"found {asd_count + td_count} (ASD = {asd_count}, TD = {td_count}) unique subjects in {src_dir}")
 
 
 
-src_dir = "../data/ABIDE/timeseries_filt_global/"
-output_filename = "../data/ABIDE/unique.json"
+src_dir = "./data/ABIDE/timeseries_filt_global/"
+output_filename = "./data/ABIDE/unique.json"
 write_unique_subject_filepaths(src_dir=src_dir, output_filename=output_filename)
 
-src_dir = "../data/generated_filt_global/pearson_corr_raw/"
-output_filename = "../data/generated_filt_global/pearson_corr_raw/unique.json"
+src_dir = "./data/generated_filt_global/pearson_corr_raw/"
+output_filename = "./data/generated_filt_global/pearson_corr_raw/unique.json"
 write_unique_subject_filepaths(src_dir=src_dir, output_filename=output_filename)
 
-src_dir = "../data/lanciano_datasets_corr_thresh_80/"
-output_filename = "../data/lanciano_datasets_corr_thresh_80/unique.json"
+src_dir = "./data/lanciano_datasets_corr_thresh_80/"
+output_filename = "./data/lanciano_datasets_corr_thresh_80/unique.json"
 write_unique_subject_filepaths(src_dir=src_dir, output_filename=output_filename)
